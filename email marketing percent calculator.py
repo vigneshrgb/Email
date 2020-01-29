b451c9ff616fc7f5d7389a4e1f06d4b9f5438918
vigneshrgb <60288495+vigneshrgb@users.noreply.github.com>
2020-01-29 22:46:40 +0530

    Update email marketing percent calculator.py

--- email marketing percent calculator.py
+++ email marketing percent calculator.py
@@ -1,12 +1,13 @@
-emails = int(input('Enter the number of Sent Emails: '))
-delivery = int(input('Enter the number of Delivered Emails: '))
-opens = int(input('Enter the number of opened emails: '))
-clicks = int(input('Enter the number of clicked emails: '))
+#Email Marketing Performance
 
-delivery_rate = (delivery / emails) * 100
-open_rate = (opens / emails) * 100
-click_rate = (clicks / emails) * 100
-print(delivery_rate)
-print(open_rate)
-print(click_rate)
+emails = int(input('Enter Emails: '))
+delivery = int(input('Enter Delivered Emails: '))
+opens = int(input('Enter opened emails: '))
+clicks = int(input('Enter clicked emails: '))
 
+delivery_rate = int((delivery / emails) * 100)
+open_rate = int((opens / emails) * 100)
+click_rate = int((clicks / emails) * 100)
+print("Delivery Rate is: "+ str(delivery_rate) + " %")
+print("Open Rate is: " + str(open_rate) + " %")
+print("Click Rate is: " + str(click_rate) + " %")
